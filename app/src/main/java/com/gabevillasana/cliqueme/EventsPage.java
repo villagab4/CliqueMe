e98704a2cf98a337535d49b8b04e234eb51164ea
Gabe V <villagab4@gmail.com>
2017-02-18 14:46:38 -0500

    Add logo and EventsPage Activity

---
+++ app/src/main/java/com/gabevillasana/cliqueme/EventsPage.java
@@ -0,0 +1,18 @@
+package com.gabevillasana.cliqueme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by Gabe Villasana on 2/18/2017.
+ */
+
+public class EventsPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events);
+    }
+
+}
