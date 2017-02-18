31ff4d68b1d9d695a36fed28860abaa6868ef9eb
Gabe V <villagab4@gmail.com>
2017-02-18 00:09:08 -0500

    Added Login Functionality

--- app/src/main/java/com/gabevillasana/cliqueme/MainActivity.java
+++ app/src/main/java/com/gabevillasana/cliqueme/MainActivity.java
@@ -1,13 +1,71 @@
 package com.gabevillasana.cliqueme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+public class MainActivity extends Activity {
+
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //INITIALIZE FACEBOOK SDK
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        //DECLARE CALLBACK METHOD
+        callbackManager = CallbackManager.Factory.create();
+
+        //LINK TEXTVIEW
+        info = (TextView) findViewById(R.id.info);
+
+        //LINK LOGIN BUTTON
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        /** Callback to handle the results of the login attempts.
+         *  onSuccess = login successful
+         *  onCancel = Canceled login
+         *  onError = Failed Login
+         */
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
