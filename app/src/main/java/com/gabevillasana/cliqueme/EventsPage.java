5ba6001695f56223c4a811071d09e2dccb22e9fa
Gabe V <villagab4@gmail.com>
2017-02-18 20:13:36 -0500

    New Button and FireBase Auth complete

--- app/src/main/java/com/gabevillasana/cliqueme/EventsPage.java
+++ app/src/main/java/com/gabevillasana/cliqueme/EventsPage.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
+import com.google.firebase.storage.FirebaseStorage;
+
 /**
  * Created by Gabe Villasana on 2/18/2017.
  */
@@ -19,5 +21,5 @@ public class EventsPage extends FragmentActivity {
         SwipeAdapter swipeAdapter = new SwipeAdapter(getSupportFragmentManager());
         viewPager.setAdapter(swipeAdapter);
     }
-
+    FirebaseStorage storage = FirebaseStorage.getInstance();
 }
--- app/src/main/java/com/gabevillasana/cliqueme/MainActivity.java
+++ app/src/main/java/com/gabevillasana/cliqueme/MainActivity.java
@@ -3,8 +3,12 @@ package com.gabevillasana.cliqueme;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import android.content.Intent;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -13,12 +17,27 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
 
     private TextView info;
     private LoginButton loginButton;
+    private FirebaseAuth mAuth;
     private CallbackManager callbackManager;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //INITIALIZE FIREBASE AUTH
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
         // DECLARE CALLBACK METHOD
         callbackManager = CallbackManager.Factory.create();
 
@@ -36,10 +73,8 @@ public class MainActivity extends Activity {
 
         // LINK LOGIN BUTTON
         loginButton = (LoginButton) findViewById(R.id.login_button);
-        /*
-        List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
+        List<String> permissionNeeds = Arrays.asList("email", "public_profile");
         loginButton.setReadPermissions(permissionNeeds);
-        */
 
         /** Callback to handle the results of the login attempts.
          *  onSuccess = login successful
@@ -49,6 +84,7 @@ public class MainActivity extends Activity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
                 launchEvents();
                 // CALL SEARCH
                 Events query = new Events(loginResult.getAccessToken());
@@ -65,6 +101,7 @@ public class MainActivity extends Activity {
                 Toast.makeText(getApplicationContext(), "Login attempt failed.", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
     @Override
@@ -76,5 +113,44 @@ public class MainActivity extends Activity {
         Intent launchEvents = new Intent(this, EventsPage.class);
         startActivity(launchEvents);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 
 }
